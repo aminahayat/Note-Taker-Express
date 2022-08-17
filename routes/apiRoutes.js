4d296fe400110fdd1ac8bc64f9b2bbafc9efe331
Amina Hayat <hayat.amina@hotmail.com>
2022-08-17 16:35:02 +0100

    completed api roite

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,6 +11,43 @@ router.get("/api/notes", (req,res)=>{
     })
 })
 
+//creates a note and adds it to the array of objects in db.json
+router.post("/api/notes", (req,res)=>{
+    fs.readFile("db/db.json", "utf8", (err, data)=>{
+        if(err) throw err;
+        const dbData = JSON.parse(data)
+        const newNote= req.body
+        const id= "id"
+        const noteId = uniqid()
+        newNote[id] = noteId
+        dbData.push(newNote)
+
+        fs.writeFile("db/db.json", JSON.stringify(dbData), (err)=>{
+            if(err) throw err
+            return res.json(dbData)
+        })
+    })
+} )
+
+router.delete("/api/notes/:id", (req,res)=>{
+    fs.readFile("db/db.json", "utf8", (err,data)=>{
+        if(err) throw err;
+        const dbData = JSON.parse(data)
+        const noteId = req.params.id
+
+        for(let i=0 ;i<dbData.length; i++){
+            if(noteId ===dbData[i].id){
+                dbData.splice([i],1)
+                fs.writeFile("db/db.json", JSON.stringify(dbData), (err)=>{
+                    if(err) throw err
+                    return res.json(dbData)
+                }) 
+            }
+        }
+
+    })
+})
+
 
 
 
