f6eb9765f1a1ad286d520d0f152b6f10774199d8
Amina Hayat <hayat.amina@hotmail.com>
2022-08-18 21:01:27 +0100

    changes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const htmlRoutes = require("./routes/htmlRoutes");
-const apiRoutes = require("./routes/apiRoutes");
+const express =require('express')
+const htmlRoutes = require("./routes/htmlRoutes")
+const apiRoutes = require("./routes/apiRoutes")
 
 const PORT = process.env.PORT || 8080;
 
 const app = express()
 
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
 
