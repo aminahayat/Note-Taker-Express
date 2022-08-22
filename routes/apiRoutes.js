72c0a10a093e38b1bd3928e428242a25209a7339
Amina Hayat <hayat.amina@hotmail.com>
2022-08-22 23:26:47 +0100

    dbi

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,50 +1,53 @@
-const express = require("express");
-const router = express.Router();
-const fs = require("fs");
-const uniqid = require("uniqid");
+const express= require("express")
+const router = express.Router()
+const fs=require("fs")
+const uniqid=require("uniqid")
 
-router.get("/api/notes", (req, res) => {
-  fs.readFile("db/db.json", "utf8", (err, data) => {
-    if (err) throw err;
-    return res.json(JSON.parse(data));
-  });
-});
+
+router.get("/api/notes", (req,res)=>{
+    fs.readFile("db/db.json", "utf8", (err,data)=>{
+        if(err) throw err
+        return res.json(JSON.parse(data))
+    })
+})
 
 //creates a note and adds it to the array of objects in db.json
-router.post("/api/notes", (req, res) => {
-  fs.readFile("db/db.json", "utf8", (err, data) => {
-    if (err) throw err;
-    const dbData = JSON.parse(data);
-    const newNote = req.body;
-    const id = "id";
-    const noteId = uniqid();
-    newNote[id] = noteId;
-    dbData.push(newNote);
+router.post("/api/notes", (req,res)=>{
+    fs.readFile("db/db.json", "utf8", (err, data)=>{
+        if(err) throw err;
+        const dbData = JSON.parse(data)
+        const newNote= req.body
+        const id= "id"
+        const noteId = uniqid()
+        newNote[id] = noteId
+        dbData.push(newNote)
 
-    fs.writeFile("db/db.json", JSON.stringify(dbData), (err) => {
-      if (err) throw err;
-      return res.json(dbData);
-    });
-  });
-});
+        fs.writeFile("db/db.json", JSON.stringify(dbData), (err)=>{
+            if(err) throw err
+            return res.json(dbData)
+        })
+    })
+} )
 
 //removes it from the array of objects
-router.delete("/api/notes/:id", (req, res) => {
-  fs.readFile("db/db.json", "utf8", (err, data) => {
-    if (err) throw err;
-    const dbData = JSON.parse(data);
-    const noteId = req.params.id;
+router.delete("/api/notes/:id", (req,res)=>{
+    fs.readFile("db/db.json", "utf8", (err,data)=>{
+        if(err) throw err;
+        const dbData = JSON.parse(data)
+        const noteId = req.params.id
 
-    for (let i = 0; i < dbData.length; i++) {
-      if (noteId === dbData[i].id) {
-        dbData.splice([i], 1);
-        fs.writeFile("db/db.json", JSON.stringify(dbData), (err) => {
-          if (err) throw err;
-          return res.json(dbData);
-        });
-      }
-    }
-  });
-});
+        for(let i=0 ;i<dbData.length; i++){
+            if(noteId ===dbData[i].id){
+                dbData.splice([i],1)
+                fs.writeFile("db/db.json", JSON.stringify(dbData), (err)=>{
+                    if(err) throw err
+                    return res.json(dbData)
+                }) 
+            }
+        }
+          
+    })
+})
 
-module.exports = router;
+
+module.exports= router;
