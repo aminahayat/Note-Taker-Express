34cd7b8fb70598464ab3a9c2ba38d09f1f61ed66
Amina Hayat <hayat.amina@hotmail.com>
2022-08-22 23:11:53 +0100

    trying to find error

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express =require('express')
-const htmlRoutes = require("./routes/htmlRoutes")
-const apiRoutes = require("./routes/apiRoutes")
+const htmlRoutes= require("./routes/htmlRoutes")
+const apiRoutes= require("./routes/apiRoutes")
 
-const PORT = process.env.PORT || 8080;
 
-const app = express()
+const PORT= process.env.PORT || 8080
+
+const app= express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
-
-app.use(htmlRoutes)
 app.use(apiRoutes)
+app.use(htmlRoutes)
 
 
-app.listen(PORT, ()=> console.log(`Listening on http://localhost:${PORT}`)) 
+
+app.listen(PORT, ()=> console.log(`Listening on http://localhost:${PORT}`))
